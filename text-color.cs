61b256d23ebe296947ae42773dfc2cbc78603957
anniejanuary <109144163+anniejanuary@users.noreply.github.com>
2022-07-20 19:04:37 +0200

    Create text-color.cs

---
+++ text-color.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("C# is cool!");
+         
+            Console.Read();
+        }
+    }
+}
